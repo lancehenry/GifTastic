2b12f56cd39f32874813d12b227f3ddc3a7ae9a0
lschmittling <lanceschmittling@Lances-MBP>
2018-02-20 16:04:47 -0600

    html changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 var key = "pqiitu4zMJkZ8McUt0eKXF5mwcAZy2AQ";
-var gifURL = "https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=" + key + "&limit=10";
+var gifURL = "https://api.giphy.com/v1/gifs/search?q=Scrubs&api_key=" + key + "&limit=10";
 console.log(gifURL);
 
 $.ajax({
@@ -8,7 +8,7 @@ $.ajax({
 }).then(function (response) {
 
     for (var i = 0; i < 10; i++) {
-        var newGif = response.data[i].images.fixed_height_small_still.url;
+        var newGif = response.data[i].images.fixed_height_still.url;
 
         var test = $("<img>").attr("src", newGif);
 
@@ -18,22 +18,21 @@ $.ajax({
     }
 
     $(".dynamic").click(function () {
-       
 
+        var clickedURL = $(this).attr("src");
+        console.log('---------------------');
+        console.log(clickedURL);
+        console.log('---------------------');
+        console.log('+++++++++++++++++++++++++');
+        var splitURL = clickedURL.split('_');
+        console.log(splitURL[0]);
+        console.log('+++++++++++++++++++++++++');
+        console.log('=========================');
+        var dynamicURL = splitURL[0] + '.gif';
+        console.log(dynamicURL);
+        console.log('=========================');
+        $(this).attr("src", dynamicURL);
 
-            var clickedURL = $(this).attr("src");
-            console.log('---------------------');
-            console.log(clickedURL);
-            console.log('---------------------');
-            console.log('+++++++++++++++++++++++++');
-            var splitURL = clickedURL.split('_');
-            console.log(splitURL[0]);
-            console.log('+++++++++++++++++++++++++');
-            console.log('=========================');
-            var dynamicURL = splitURL[0] + '.gif';
-            console.log(dynamicURL);
-            console.log('=========================');
-            $(this).attr("src", dynamicURL);
     })
 
 })
